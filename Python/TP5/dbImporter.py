953681f7940901eff5b4821605cfe247bc08e7c9
SeimuPVE <paul.vanelsue@gmail.com>
2018-10-16 10:02:43 +0200

    Updating Python TP5.

--- Python/TP5/dbImporter.py
+++ Python/TP5/dbImporter.py
@@ -8,20 +8,18 @@ class dbImporter:
         self.db = sqlite3.connect('tp5.db')
 
     def readCommunes(self, filename):
-        total_err = 0
-        total_done = 0
         # Create table.
         cursor = self.db.cursor()
         cursor.execute("""
         CREATE TABLE IF NOT EXISTS communes(
              id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
              codeRegion INTEGER,
-             nomRegion TEXT,
-             codeDepartement INTEGER,
+             nomRegion VARCHAR,
+             codeDepartement VARCHAR,
              codeArrondissement INTEGER,
              codeCanton INTEGER,
              codeCommune INTEGER,
-             nomCommune TEXT,
+             nomCommune VARCHAR,
              populationMunicipale INTEGER,
              populationCompteeAPart INTEGER,
              populationTotale INTEGER
@@ -34,14 +32,14 @@ class dbImporter:
                 commune = commune.decode("Windows-1252")
                 commune = commune.split(";")
                 try:
-                    if(int(commune[0]) >= 0 and int(commune[0]) <= 9):
-                        for i in [0, 2, 3, 4, 5, 7, 8, 9]:
+                    if(int(commune[0]) >= 0 and int(commune[0]) <= 99):
+                        for i in [2, 3, 4, 5, 7, 8, 9]:
                             if " " in commune[i]:
-                                commune[i].replace(' ', '')
+                                commune[i] = commune[i].replace(' ', '')
                         data = {
                             "codeRegion": int(commune[0]),
                             "nomRegion": commune[1],
-                            "codeDepartement": int(commune[2]),
+                            "codeDepartement": commune[2],
                             "codeArrondissement": int(commune[3]),
                             "codeCanton": int(commune[4]),
                             "codeCommune": int(commune[5]),
@@ -72,10 +70,124 @@ class dbImporter:
                                 :populationCompteeAPart,
                                 :populationTotale)
                         """, data)
-                        self.db.commit()
-                        total_done = total_done + 1
                 except Exception as e:
-                    print(e)
-                    total_err = total_err + 1
-                print("Total err : " + str(total_err))
-                print("Total done : " + str(total_done))
+                    pass
+            self.db.commit()
+
+    def readDepartements(self, filename):
+        # Create table.
+        cursor = self.db.cursor()
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS departements(
+             id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+             codeRegion INTEGER,
+             nomRegion VARCHAR,
+             codeDepartement VARCHAR,
+             nomDepartement VARCHAR,
+             nombreArrondissements INTEGER,
+             nombreCantons INTEGER,
+             nombreCommunes INTEGER,
+             populationMunicipale INTEGER,
+             populationTotale INTEGER
+        )
+        """)
+        self.db.commit()
+        # Read CSV and insert data into the DB.
+        with open(filename, 'rb') as file:
+            for departement in file.readlines():
+                departement = departement.decode("Windows-1252")
+                departement = departement.split(";")
+                try:
+                    if(int(departement[0]) >= 0 and int(departement[0]) <= 99):
+                        for i in [0, 4, 5, 6, 7, 8, 9]:
+                            if " " in departement[i]:
+                                departement[i] = departement[i].replace(' ', '')
+                        data = {
+                            "codeRegion": int(departement[0]),
+                            "nomRegion": departement[1],
+                            "codeDepartement": departement[2],
+                            "nomDepartement": departement[3],
+                            "nombreArrondissements": int(departement[4]),
+                            "nombreCantons": int(departement[5]),
+                            "nombreCommunes": int(departement[6]),
+                            "populationMunicipale": int(departement[7]),
+                            "populationTotale": int(departement[8]),
+                        }
+                        cursor.execute("""
+                        INSERT INTO departements(   codeRegion,
+                                                    nomRegion,
+                                                    codeDepartement,
+                                                    nomDepartement,
+                                                    nombreArrondissements,
+                                                    nombreCantons,
+                                                    nombreCommunes,
+                                                    populationMunicipale,
+                                                    populationTotale)
+                        VALUES( :codeRegion,
+                                :nomRegion,
+                                :codeDepartement,
+                                :nomDepartement,
+                                :nombreArrondissements,
+                                :nombreCantons,
+                                :nombreCommunes,
+                                :populationMunicipale,
+                                :populationTotale)
+                        """, data)
+                except Exception as e:
+                    pass
+            self.db.commit()
+
+    def readRegions(self, filename):
+        # Create table.
+        cursor = self.db.cursor()
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS regions(
+             id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+             codeRegion INTEGER,
+             nomRegion VARCHAR,
+             nombreArrondissements INTEGER,
+             nombreCantons INTEGER,
+             nombreCommunes INTEGER,
+             populationMunicipale INTEGER,
+             populationTotale INTEGER
+        )
+        """)
+        self.db.commit()
+        # Read CSV and insert data into the DB.
+        with open(filename, 'rb') as file:
+            for region in file.readlines():
+                region = region.decode("Windows-1252")
+                region = region.split(";")
+                try:
+                    if(int(region[0]) >= 0 and int(region[0]) <= 99):
+                        for i in [0, 2, 3, 4, 5, 6]:
+                            if " " in region[i]:
+                                region[i] = region[i].replace(' ', '')
+                        data = {
+                            "codeRegion": int(region[0]),
+                            "nomRegion": region[1],
+                            "nombreArrondissements": int(region[2]),
+                            "nombreCantons": int(region[3]),
+                            "nombreCommunes": int(region[4]),
+                            "populationMunicipale": int(region[5]),
+                            "populationTotale": int(region[6])
+                        }
+                        cursor.execute("""
+                        INSERT INTO regions (   codeRegion,
+                                                nomRegion,
+                                                nombreArrondissements,
+                                                nombreCantons,
+                                                nombreCommunes,
+                                                populationMunicipale,
+                                                populationTotale)
+                        VALUES( :codeRegion,
+                                :nomRegion,
+                                :nombreArrondissements,
+                                :nombreCantons,
+                                :nombreCommunes,
+                                :populationMunicipale,
+                                :populationTotale)
+                        """, data)
+                except Exception as e:
+                    pass
+            self.db.commit()
--- Python/TP5/main.py
+++ Python/TP5/main.py
@@ -2,11 +2,26 @@ from dbImporter import *
 
 
 dbImporter = dbImporter()
-dbImporter.readCommunes("bdd/communes.csv")
+#dbImporter.readCommunes("bdd/communes.csv")
+#dbImporter.readDepartements("bdd/departements.csv")
+#dbImporter.readRegions("bdd/regions.csv")
+print("Question 1 : database up !")
 
 db = sqlite3.connect('tp5.db')
 cursor = db.cursor()
-cursor.execute("""SELECT * FROM communes""")
 
+print("Question 2 :")
+print("Population totale des communes :")
+cursor.execute("""SELECT nomCommune, populationTotale FROM communes""")
 for commune in cursor.fetchall():
     print(commune)
+
+print("Population totale des régions :")
+cursor.execute("""SELECT nomRegion, populationTotale FROM regions""")
+for region in cursor.fetchall():
+    print(region)
+
+print("Question 3 : TODO")
+cursor.execute("""SELECT DISTINCT nomCommune, codeDepartement FROM communes c1 WHERE EXISTS (SELECT * FROM communes c2 WHERE c1.nomCommune <> c2.nomCommune)""")
+for doublon in cursor.fetchall():
+    print(doublon)
