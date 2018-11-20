7a69ecf274626c5ce83d95184359d3fc9cfed4f8
SeimuPVE <paul.vanelsue@gmail.com>
2018-11-20 10:38:27 +0100

    Adding partialy M2M TP (doesn't work).

--- M2M/ShieldEthernet/src/main.ino
+++ M2M/ShieldEthernet/src/main.ino
@@ -1,10 +1,16 @@
 #include <SPI.h>
 #include <Ethernet.h>
+#include "Seeed_BME280.h"
+#include <Wire.h>
 
 byte mac[] = {0x90, 0xA2, 0xDA, 0x0F, 0xFB, 0xBE};
 IPAddress ip(192, 168, 0, 1);
-
 EthernetServer server(80);
+
+BME280 bme280;
+
+String buffer;
+char c;
 
 
 void setup() {
@@ -21,57 +27,88 @@ void setup() {
 
     Serial.print("Server is at ");
     Serial.println(Ethernet.localIP());
+
+    if(!bme280.init())
+        Serial.println("Error with BME280 !");
 }
 
-
 void loop() {
-//    EthernetClient client = server.available();
-//
-//    if (client) {
-//        Serial.println("new client");
-//        // an http request ends with a blank line
-//        bool currentLineIsBlank = true;
-//        while (client.connected()) {
-//            if (client.available()) {
-//                char c = client.read();
+    EthernetClient client = server.available();
+
+    if (client) {
+        Serial.println("New client");
+
+        // An http request ends with a blank line.
+        bool currentLineIsBlank = true;
+
+        while(client.connected()) {
+            if(client.available()) {
+                buffer = ""
+                c = client.read();
 //                Serial.write(c);
-//                // if you've gotten to the end of the line (received a newline
-//                // character) and the line is blank, the http request has ended,
-//                // so you can send a reply
-//                if (c == '\n' && currentLineIsBlank) {
-//                    // send a standard http response header
-//                    client.println("HTTP/1.1 200 OK");
-//                    client.println("Content-Type: text/html");
-//                    client.println("Connection: close");  // the connection will be closed after completion of the response
-//                    client.println("Refresh: 5");  // refresh the page automatically every 5 sec
-//                    client.println();
-//                    client.println("<!DOCTYPE HTML>");
-//                    client.println("<html>");
-//                    // output the value of each analog input pin
-//                    for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
-//                        int sensorReading = analogRead(analogChannel);
-//                        client.print("analog input ");
-//                        client.print(analogChannel);
-//                        client.print(" is ");
-//                        client.print(sensorReading);
-//                        client.println("<br />");
-//                    }
-//                    client.println("</html>");
-//                    break;
-//                }
-//                if (c == '\n') {
-//                    // you're starting a new line
-//                    currentLineIsBlank = true;
-//                } else if (c != '\r') {
-//                    // you've gotten a character on the current line
-//                    currentLineIsBlank = false;
-//                }
-//            }
-//        }
-//        // give the web browser time to receive the data
-//        delay(1);
-//        // close the connection:
-//        client.stop();
-//        Serial.println("client disconnected");
-//    }
+                Serial.println(buffer);
+
+                // If you've gotten to the end of the line (received a newline character) and the line is blank, the http request has ended, so you can send a reply.
+                if(c == '\n' && currentLineIsBlank) {
+                    client.println("HTTP/1.1 200 OK");
+                    client.println("Content-Type: text/html");
+                    client.println("Connection: close");
+                    client.println("Refresh: 5");
+                    client.println();
+                    client.println("<!DOCTYPE HTML>");
+                    client.println("<meta charset=\"UTF-8\"> ");
+                    client.println("<html>");
+
+                    // Output the value of each analog input pin.
+                    // Get and print temperatures.
+                    client.println("<section>");
+                    client.print("<div>Temperature : ");
+                    client.print(bme280.getTemperature());
+                    client.println(" °C</div>");
+
+                    // Get and print atmospheric pressure data.
+                    client.print("<div>Pressure : ");
+                    client.print(bme280.getPressure() / 100);
+                    client.println(" hPa</div>");
+
+                    // Get and print altitude data.
+                    client.print("<div>Altitude: ");
+                    client.print(bme280.calcAltitude(bme280.getPressure()));
+                    client.println("m</div>");
+
+                    // Get and print humidity data.
+                    client.print("<div>Humidity: ");
+                    client.print(bme280.getHumidity());
+                    client.println("%</div>");
+                    client.println("</section>");
+
+
+                    // Add three buttons to control the output led.
+                    client.println("<section>");
+                    client.println("<form method=\"GET\">");
+                    client.println("<input type=\"submit\" name=\"led_control\" value=\"red\">");
+                    client.println("<input type=\"submit\" name=\"led_control\" value=\"green\">");
+                    client.println("<input type=\"submit\" name=\"led_control\" value=\"blue\">");
+                    client.println("</form>");
+                    client.println("</section>");
+                    client.println("</html>");
+                    break;
+                }
+
+                if(c == '\n') // You're starting a new line.
+                    currentLineIsBlank = true;
+                else if (c != '\r') { // You've gotten a character on the current line.
+                    buffer += c;
+                    currentLineIsBlank = false;
+                }
+            }
+        }
+
+        // Give the web browser time to receive the data.
+        delay(1);
+
+        // Close the connection.
+        client.stop();
+        Serial.println("client disconnected");
+    }
 }
