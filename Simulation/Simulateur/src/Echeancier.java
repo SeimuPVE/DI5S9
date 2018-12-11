8e34e42a7861bd5976160adeab3c7afb28b67075
GouVsky <greg6-marco@hotmail.fr>
2018-12-11 14:40:01 +0100

    Adding of ajouterEvenement.

--- Simulation/Simulateur/src/Echeancier.java
+++ Simulation/Simulateur/src/Echeancier.java
@@ -3,12 +3,17 @@ import java.util.ArrayList;
 
 public class Echeancier
 {
-    List <Evenement> evenements;
+    private List <Evenement> evenements;
 
 
     public Echeancier()
     {
         evenements = new ArrayList <Evenement> ();
+    }
+
+    public void addEvenement(Evenement evenement)
+    {
+        evenements.add(evenement);
     }
 
     public List <Evenement> getEvenements()
