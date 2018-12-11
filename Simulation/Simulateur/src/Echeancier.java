73e2a9d14224a0ac851968e90eaf41e634dabf23
GouVsky <greg6-marco@hotmail.fr>
2018-12-11 14:38:00 +0100

    Adding of Echeancier class.

---
+++ Simulation/Simulateur/src/Echeancier.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+
+public class Echeancier
+{
+    List <Evenement> evenements;
+
+
+    public Echeancier()
+    {
+        evenements = new ArrayList <Evenement> ();
+    }
+
+    public List <Evenement> getEvenements()
+    {
+        return evenements;
+    }
+}
