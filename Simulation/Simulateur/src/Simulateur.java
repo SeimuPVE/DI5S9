db2409d6f40fc5e72822558267a61d5795fbe814
SeimuPVE <paul.vanelsue@gmail.com>
2018-12-11 14:31:26 +0100

    Adding structure for the simulator.

---
+++ Simulation/Simulateur/src/Simulateur.java
@@ -0,0 +1,33 @@
+public class Simulateur {
+
+    public Simulateur() {
+    }
+
+    public void run() {
+
+    }
+
+    public void debut() {
+
+    }
+
+    public void fin() {
+
+    }
+
+    public void arrClient() {
+
+    }
+
+    public void accesFileTelephonique() {
+
+    }
+
+    public void accesAppel() {
+
+    }
+
+    public void finAppel() {
+
+    }
+}
