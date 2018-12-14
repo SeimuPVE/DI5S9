d9e3991a5d34ce1cd1a720bb4fe606319dd51804
SeimuPVE <paul.vanelsue@gmail.com>
2018-12-14 17:48:42 +0100

    Doing main.

--- Simulation/Simulateur/src/Main.java
+++ Simulation/Simulateur/src/Main.java
@@ -1,16 +1,9 @@
 import util.Simulateur;
 
-/*
- * util.Echeancier : couple événements/heures trié par heures
- *
- * Evenement : classe abstraite
- * Chaque événement a sa classe
- * ArrClient parallélisé
- *
- * Chaque événément lance son événement suivant
- */
+
 public class Main {
     public static void main(String[] args) {
         Simulateur simulateur = new Simulateur(500);
+        simulateur.run();
     }
 }
