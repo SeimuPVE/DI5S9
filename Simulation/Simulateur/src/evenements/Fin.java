258bddbbe472a182ffb6214649fa31402ab17c33
ywlqjl <yanwenliqjl@gmail.com>
2018-12-17 16:43:03 +0100

    Add fin

--- Simulation/Simulateur/src/evenements/Fin.java
+++ Simulation/Simulateur/src/evenements/Fin.java
@@ -1,9 +1,13 @@
 package evenements;
 
 
+import util.Simulateur;
+
 public class Fin extends Evenement {
     @Override
     public void run() {
         // PAGRANULEUX.
+        System.out.println("Temps d’attente maximum :"+ Simulateur.getTempsActuel());
+        System.out.println("Temps d’attente moyen :" + Simulateur.getAttGlb() / Simulateur.getN());
     }
 }
