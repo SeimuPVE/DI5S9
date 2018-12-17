f6d32d4cd76030d65b6213dc4e3617334c83c1ec
Unknown <penghanyuan@163.com>
2018-12-17 16:30:39 +0100

    add limite

--- Simulation/Simulateur/src/evenements/ArrClient.java
+++ Simulation/Simulateur/src/evenements/ArrClient.java
@@ -21,19 +21,22 @@ public class ArrClient extends Evenement implements Runnable {
         long interArrivee = (long) LoiSimulateur.loi_exp(0.4);
 
         // Si la simulation n'est pas fini
+        if (Simulateur.getTempsActuel() < Simulateur.getT()) {
+            // arrivee client
+            Clients.ajouterClient(new Client(Simulateur.getTempsActuel()));
 
-        // arrivee client
-        Clients.ajouterClient(new Client(Simulateur.getTempsActuel()));
 
+            AccFileTelephonique accFileTelephonique = new AccFileTelephonique();
+            // creer AccFileTelephonique
+            Echeancier.ajouterEvenement(accFileTelephonique, Simulateur.getTempsActuel());
 
-        AccFileTelephonique accFileTelephonique = new AccFileTelephonique();
-        // creer AccFileTelephonique
-        Echeancier.ajouterEvenement(accFileTelephonique, Simulateur.getTempsActuel());
-        
-        ArrClient arrClient = new ArrClient(Simulateur.getTempsActuel() + interArrivee);
-        Echeancier.ajouterEvenement(arrClient, Simulateur.getTempsActuel() + interArrivee);
-        arrClient.run();
-        accFileTelephonique.run();
+            ArrClient arrClient = new ArrClient(Simulateur.getTempsActuel() + interArrivee);
+            Echeancier.ajouterEvenement(arrClient, Simulateur.getTempsActuel() + interArrivee);
+
+            arrClient.run();
+            accFileTelephonique.run();
+        }
+
 
     }
 }
