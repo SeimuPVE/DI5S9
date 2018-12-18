b3c43deff1d122595b7e6323457b68bcd98cb359
SeimuPVE <paul.vanelsue@gmail.com>
2018-12-18 16:21:39 +0100

    Updating TP6.

--- SystemesMultimedias/TP6/client.py
+++ SystemesMultimedias/TP6/client.py
@@ -6,8 +6,8 @@ import sys
 
 
 # Initialize variables.
-UDP_IP = int(sys.argv[0])
-UDP_PORT = int(sys.argv[1])
+UDP_IP = str(sys.argv[1])
+UDP_PORT = int(sys.argv[2])
 BUFFER_SIZE = 4096
 WIDTH = 320
 HEIGHT = 180
@@ -15,17 +15,26 @@ HEIGHT = 180
 
 # Initialize the socket.
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Internet with UDP.
-sock.connect((UDP_IP, UDP_PORT))
 
 
 # Receive the data.
-raw_img = []
-while True:
-    data = sock.recv(BUFFER_SIZE)
-    if not data:
-        break
-    raw_img.append(data)
+sock.sendto(bytes(0), (UDP_IP, UDP_PORT))
 
+img_size, server = sock.recvfrom(8)
+img_size = int.from_bytes(img_size, byteorder='big')
+print("An image of " + str(img_size) + " bytes will be received.")
+
+raw_img = bytes()
+while len(raw_img) < img_size:
+    if img_size - len(raw_img) < BUFFER_SIZE:
+        data, server = sock.recvfrom(img_size - len(raw_img))
+        raw_img = raw_img + data
+    else:
+        data, server = sock.recvfrom(BUFFER_SIZE)
+        raw_img = raw_img + data
+    print(len(raw_img))
+
+print("A")
 
 # Convert the data in OpenCV img.
 im = np.frombuffer(raw_img, dtype='int8')  # Convert bytes to numpy format.
@@ -35,3 +44,7 @@ im = np.reshape(im, (WIDTH, HEIGHT))  # Convert 1D raw into matrix.
 
 # Print the image to the screen.
 plt.imshow(im)
+
+
+# Close the socket.
+sock.close()
--- SystemesMultimedias/TP6/server.py
+++ SystemesMultimedias/TP6/server.py
@@ -6,7 +6,16 @@ import time
 import socket
 
 
+def int2bytes(n):
+    result = bytearray()
+    while n:
+        result.append(n & 0xff)
+        n = n >> 8
+    return result[::-1]
+
+
 # Initialize variables.
+SERVER_ADDR = "192.168.1.253"
 PORT = 5006
 BUFFER_SIZE = 4096
 WIDTH = 320
@@ -24,10 +33,8 @@ time.sleep(0.1)  # Allow the camera to warm up.
 
 
 # Initialize the socket.
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Internet with UDP.
-server_socket.listen(10)
-sockfd, client_address = server_socket.accept()
-print("A new client is connected : " + client_address)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Internet with UDP.
+sock.bind((SERVER_ADDR, PORT))
 
 
 # Capture frames from the camera.
@@ -36,20 +43,27 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     image = frame.array
     image.flags.writeable = True
 
-    cv2.imshow("Frame", image)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray = cv2.resize(gray, (WIDTH, HEIGHT), interpolation=cv2.INTER_LINEAR)
 
     # Send the image to the client.
-    while gray.size != 0:
-        if gray.size < BUFFER_SIZE:
+    print("Waiting on a client...")
+    data, address = sock.recvfrom(8)
+    sock.sendto(int2bytes(gray.size), address)
+    print("A new client is connected. An image of " + str(gray.size) + " bytes will be send.")
+
+    while int(gray.size) > 0:
+        if int(gray.size) < BUFFER_SIZE:
+            print("Last")
             data = gray[0:gray.size]
-            gray = gray[gray.size:]
-            sockfd.send(data)
+            gray = []
+            sock.sendto(data, address)
         else:
+            print("In")
             data = gray[0:BUFFER_SIZE]
             gray = gray[BUFFER_SIZE:]
-            sockfd.send(data)
+            sock.sendto(data, address)
+        print(gray.size)
 
     # Clear the stream in preparation for the next frame.
     rawCapture.truncate(0)
@@ -59,5 +73,5 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     if key == ord("q"):
         break
 
-sockfd.close()
+sock.close()
 camera.close()
